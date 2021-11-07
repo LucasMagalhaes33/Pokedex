8127841f27e9aca64529c7e2d64df4fcdd2a00c9
Lucas Magalhaes <lucas.magalhaes911@gmail.com>
2021-11-07 15:49:39 -0300

    consume api

--- src/app/shared/poker-list/poker-list.component.ts
+++ src/app/shared/poker-list/poker-list.component.ts
@@ -14,7 +14,7 @@ export class PokerListComponent implements OnInit {
 
   ngOnInit(): void {
     this.pokeApiService.apiListAllPokemons.subscribe(
-      res => res
+      res => console.log(res)
     )
 
   }
